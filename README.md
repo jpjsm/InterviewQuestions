2edbac20b1e64fe9f9fa3fce60d0759dab1bc5e0
Juan Pablo 'JP' Jofre <juanpablo.jofre@live.com>
2025-07-12 21:53:27 -0700

    Added knowledge

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# /repos/jpjsm@github/InterviewQuestions
+# Interview Questions
+
+This is the place where I have collected most of the interview questions
+that I had asked to candidates, or that have been asked to me as a candidate.
+
+It's hard to tell the origin of some of the questions, as I saved the question
+from the interview (without stating the source a the moment I saved the question).
+Some of these questions are 15+ years old; therefore, impossible to track down the source.
+
+> **My apologies to the authors of these questions for not mentioning them!!**
+
+What I can do is mention the source books that I have used over the years to prepare
+for the interviews that I had taken, whether they are interview books, or text books.
+
+About the interview questions that I had used to interview candidates...
+Something similar had happened here; some of the interview questions were taken 
+from the books I have (i.e. the check for palindrome, or find the all the anagram groups
+from a list of words), but the questions were tweaked to measure something else and
+they differ from the original in any of the sources.
+
+I'll do my best to search in my sources and give proper credit to the authors while
+reorganizing this repo.
+
+## Bibliography
+
+| Title | Author(s) | Other details |
+| :-: | :-: | :- |
+| Algorithmic Puzzles | Anany Levitin; Maria Levitin | ISBN: 9780199740444, ©2011|
+| Mazes for Programmers | James Buck | ISBN: 9781680500554, ©2015 |
+| Algorithms For Interviews | Adnan Aziz; Amit Prakash | ISBN: 9781453792995, ©2010 |
+
---
+++ original-sources/bitbucket/interview-questions/Q23 - FindClosestValue/CS/FindClosestValue.cs
@@ -0,0 +1,132 @@
+﻿
+namespace FindClosestValue
+{
+    public class Node
+    {
+        private int _value;
+        private Node? _left = null;
+        private Node? _right = null;
+
+        public int Value => _value;
+        public Node? Left
+        {
+            get { return _left; }
+            set
+            {
+                if (Object.ReferenceEquals(this, value)) throw new ArgumentOutOfRangeException("Circular (infinite) reference.");
+                _left = value;
+            }
+        }
+        public Node? Right
+        {
+            get { return _right; }
+            set
+            {
+                if (Object.ReferenceEquals(this, value)) throw new ArgumentOutOfRangeException("Circular (infinite) reference.");
+                _right = value;
+            }
+        }
+
+        public Node(int value)
+        {
+            _value = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node_13 = new Node(13);
+            Node node_23 = new Node(23);
+            Node node_47 = new Node(47);
+            Node node_71 = new Node(71);
+            Node node_97 = new Node(97);
+
+            Node node_89 = new Node(89);
+            node_89.Right = node_97;
+            node_89.Left = node_71;
+
+            Node node_61 = new Node(61);
+            node_61.Left = node_47;
+            node_61.Right = node_89;
+
+            Node node_7 = new Node(7);
+            node_7.Right = node_13;
+
+            Node node_29 = new Node(29);
+            node_29.Left = node_23;
+
+            Node node_17 = new Node(17);
+            node_17.Left = node_7;
+            node_17.Right = node_29;
+
+
+            Node root = new Node(31);
+            root.Left = node_17;
+            root.Right = node_61;
+
+            (int target, int expected)[] testCases = new (int target, int expected)[]
+            {
+                new(36,31),
+                new(24,23),
+                new (6,7),
+                new (98,97),
+                new (30,31),
+                new (26,29),
+                new (14,13),
+                new (15,17),
+                new (16,17),
+            };
+
+            Console.WriteLine("Begin Task");
+            Console.WriteLine(new string('*',120));
+            foreach ((int target, int expected) testCase in testCases)
+            {
+                int actual = FindClosestValue(testCase.target, root);
+                if (actual != testCase.expected)
+                {
+                    Console.WriteLine($"failed test case for target '{testCase.target}': actual -> {actual} vs {testCase.expected} <- expected");
+                }
+                else
+                {
+                    Console.WriteLine($"good job! target '{testCase.target}': closest -> {actual}");
+                }
+            }
+
+
+            Console.WriteLine(new string('=',120));
+            Console.WriteLine("Task Completed");
+        }
+
+        public static int FindClosestValue(int target, Node root)
+        {
+            int minDistance = Math.Abs(target - root.Value);
+            int closestValue = root.Value;
+            Node? current = root;
+            int distance;
+            while (current != null)
+            {
+                distance = Math.Abs(target - current.Value);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestValue = current.Value;
+                }
+
+                if (target == current.Value) return current.Value;
+
+                if (target < current.Value)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                 }
+            }
+
+            return closestValue;
+        }
+    }
+}
