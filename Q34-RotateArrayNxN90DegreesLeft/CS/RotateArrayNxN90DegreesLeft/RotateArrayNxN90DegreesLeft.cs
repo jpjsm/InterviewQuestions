48a56e8a2deec2b16cf4740381136818e9168ba2
Juan Pablo 'JP' Jofre <juanpablo.jofre@live.com>
2025-07-18 13:37:55 -0700

    Completed method documentation

--- Q34-RotateArrayNxN90DegreesLeft/CS/RotateArrayNxN90DegreesLeft/RotateArrayNxN90DegreesLeft.cs
+++ Q34-RotateArrayNxN90DegreesLeft/CS/RotateArrayNxN90DegreesLeft/RotateArrayNxN90DegreesLeft.cs
@@ -2,6 +2,27 @@
 
 public static class RotateArray
 {
+    /// <summary>
+    /// Rotate square array 90° left.
+    /// </summary>
+    /// <remarks>
+    /// - This method rotates the array in place.
+    /// - This method takes O(n) time to execute; where n is the total number of cells in the array.
+    /// - This method takes O(1) space to execute; the extra space used is one integer (tmp).
+    /// </remarks>
+    /// <remarks>
+    /// Algorithm:
+    /// Imagine the squared array as a collection of concentric rings.
+    /// Rotate each ring 90° left.
+    /// At each ring, cells are paired in groups of 4 cells.
+    /// move the first cell of four to 'tmp'
+    /// move the 2nd to 1st, 3rd to 2nd, 4th to 3rd
+    /// move 'tmp' to 4th
+    /// </remarks>
+    /// <param name="A">The square array to rotate.</param>
+    /// <returns>The rotated array.</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public static int[,] RotateArrayNxN90DegreesLeft(int[,] A)
     {
         ArgumentNullException.ThrowIfNull(A);
