6ba63762eb75eab9b4843627846a335ddafca011
JP Jofre <juanpablo.jofre@live.com>
2026-01-15 14:18:40 -0800

    Find Fist Possitive MIssing Integer: code complete and tested

---
+++ Q55-FirstMissingInteger/README.md
@@ -0,0 +1,12 @@
+# Find the First Missing Positive
+
+Given an unsorted integer array, find the first missing positive integer. Zero
+is not part of possitive integers.
+
+For example,
+
+- Given [1, 2, 0] return 3,
+- Given [3, 4, -1, 1] return 2.
+- Given [4, 8, 2, 1, 0, -1, 5] return 3.
+
+The solution should run in O(n) time and uses constant space.
---
+++ Q55-FirstMissingInteger/python/findfirstmissinginteger.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+def FindFistMissingInteger(numbers: List[int]) -> int:
+    i = 0
+    j = len(numbers) - 1
+    N = i + 1
+    while i <= j:
+        if numbers[i] == N:
+            # Number is in the right place, move to next
+            i += 1
+            N = i + 1
+        elif numbers[i] < 1 or numbers[i] >= j:
+            # swap with the number at the current end -> numbers[j]
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            j -= 1
+        else:
+            while numbers[i] > N and numbers[i] <= j:
+                t = numbers[i]
+                numbers[t - 1], numbers[i] = numbers[i], numbers[t - 1]
+    return N
+
+
+if __name__ == "__main__":
+    tests = [
+        ([1, 2, 3, 4, 5], 6),
+        ([1, 2, 0], 3),
+        ([3, 4, -1, 1], 2),
+        ([1, 2, 3, 4, 9], 5),
+        ([4, 3, 2, 1, 0, -1, 5], 6),
+        ([4, 8, 2, 1, 0, -1, 5], 3),
+    ]
+    for t, e in tests:
+        r = FindFistMissingInteger(t)
+        if e != r:
+            print(f"ERROR: expected {e} != {r} result")
+    print("Tests completed!")
